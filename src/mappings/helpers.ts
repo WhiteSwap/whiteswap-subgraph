552fe65363bec9f0b410de0c1d2ef0ea7984e64d
Nick <nick@MBP-Nick.mshome.net>
2020-11-10 11:05:14 +0200

    Update subgraph contracts.

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -12,7 +12,7 @@ import {
 } from './tokenList'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xed6FF6254E5B0C57DE2389A19C1a09cAD16Fe318'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
